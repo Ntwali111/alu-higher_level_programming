e2ca97925b38295681420308cc72a5438d005a61
Ntwali111 <u.cynthia1@alustudent.com>
2024-09-26 12:42:52 +0000

    11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,33 +1,25 @@
 #!/usr/bin/node
-
-// Function to find the second biggest integer in an array of integers
-function findSecondBiggest (arr) {
-  // Initialize two variables to hold the biggest and second biggest values
-  let biggest = -Infinity;
-  let secondBiggest = -Infinity;
-
-  // Loop through the array to find the biggest and second biggest values
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > biggest) {
-      secondBiggest = biggest;
-      biggest = arr[i];
-    } else if (arr[i] > secondBiggest && arr[i] < biggest) {
-      secondBiggest = arr[i];
-    }
-  }
-
-  // Return the second biggest value
-  return secondBiggest;
-}
-
-// Get the arguments from the command line, excluding the first two elements (node and script name)
-const args = process.argv.slice(2).map(Number);
-
-// Check the length of the arguments array
-if (args.length <= 1) {
+const myArgs = process.argv.splice(2);
+if (myArgs.length === 0 || myArgs.length === 1) {
   console.log(0);
 } else {
-  // Find and print the second biggest integer
-  const result = findSecondBiggest(args);
-  console.log(result === -Infinity ? 0 : result);
+  let i = 0;
+  let max = 0;
+  let result = 0;
+  if (Number(myArgs[0]) > Number(myArgs[1])) {
+    max = myArgs[0];
+    result = myArgs[1];
+  } else {
+    max = myArgs[1];
+    result = myArgs[0];
+  }
+  for (i = 2; i < myArgs.length; i++) {
+    if (Number(myArgs[i]) >= Number(max)) {
+      result = max;
+      max = myArgs[i];
+    } else if (Number(myArgs[i]) >= Number(result)) {
+      result = myArgs[i];
+    }
+  }
+  console.log(result);
 }
