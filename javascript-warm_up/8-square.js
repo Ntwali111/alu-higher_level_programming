b6eaa959654057e999a11f009b63b4a1e8cb820c
Ntwali111 <u.cynthia1@alustudent.com>
2024-09-26 12:05:59 +0000

    8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,24 +1,20 @@
 #!/usr/bin/node
+
+// Parse the first argument passed to the script and convert it to an integer
 const size = parseInt(process.argv[2]);
-if (!isNaN(size)) {
+
+// Check if the argument is a valid number and greater than zero
+if (isNaN(size) || size <= 0) {
+  // If the argument is not a number or less than or equal to zero, print an error message
+  console.log('Missing size');
+} else {
+  // If the argument is a valid positive number, print the square
   for (let i = 0; i < size; i++) {
+    // Print a row of 'X' characters
     let row = '';
     for (let j = 0; j < size; j++) {
       row += 'X';
     }
     console.log(row);
   }
-} else {
-  console.log('Missing size');
-}t size = parseInt(process.argv[2]);
-if (!isNaN(size)) {
-  for (let i = 0; i < size; i++) {
-    let row = '';
-    for (let j = 0; j < size; j++) {
-      row += 'X';
-    }
-    console.log(row);
-  }
-} else {
-  console.log('Missing size');
 }
