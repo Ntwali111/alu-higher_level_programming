50ddbc66c772baeeac7904a267d9b7e510ea0514
Ntwali111 <u.cynthia1@alustudent.com>
2024-10-07 16:51:43 +0000

    9-script.js

---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // Fetch data from the API
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function(data) {
+    // Display the translation of "hello" in the DIV#hello
+    $('#hello').text(data.hello);
+  });
+});
